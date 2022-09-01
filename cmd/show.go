a4897b506fbc6c43b1596e9780896fd0c07c862f
zhanglianx111 <zhanglianx111@aliyun.com>
2022-09-01 19:23:34 +0800

    show command

---
+++ cmd/show.go
@@ -0,0 +1,53 @@
+package cmd
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/spf13/cobra"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var showCmd = &cobra.Command{
+	Use:   string("show"),
+	Short: string("显示主机用户名和密码信息"),
+	Long:  string("\n显示主机用户名和密码信息"),
+	Run: func(cmd *cobra.Command, args []string) {
+		showOne()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(showCmd)
+}
+
+func showOne() {
+	machines := getAll()
+	show(machines)
+
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("请输入要显示主机信息的序号: ")
+		text, err := reader.ReadString('\n')
+		if err != nil {
+			log.Println(err.Error())
+			return
+		}
+
+		// show machine
+		choose := strings.Split(text, "\n")[0]
+		if 0 == len(choose) {
+			return
+		}
+
+		index, err := strconv.Atoi(choose)
+		if err != nil {
+			log.Println(err.Error())
+			return
+		}
+
+		fmt.Printf("user: %s, password: %s\n\n", machines[index].User, machines[index].Password)
+	}
+}
